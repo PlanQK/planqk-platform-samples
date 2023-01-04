ab4e16376b17679df24cf34ec7ebe9058a596df2
valentin.aponte.sarmientos <valentin.aponte.sarmientos@stoneone.de>
2023-01-04 18:38:51 +0100

    add java samples for taxonomy elements

---
+++ java/planqk-samples/app/src/main/java/de/stoneone/planqk/samples/AlgorithmAddAndDeleteApplicationAreasSample.java
@@ -0,0 +1,70 @@
+package de.stoneone.planqk.samples;
+
+import de.stoneone.planqk.api.CommunityAlgorithmsApi;
+import de.stoneone.planqk.api.invoker.ApiClient;
+import de.stoneone.planqk.api.model.AlgorithmDto;
+import de.stoneone.planqk.api.model.TaxonomyElement;
+import de.stoneone.planqk.api.model.UpdateAlgorithmRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgorithmAddAndDeleteApplicationAreasSample {
+
+    public static void main(String[] args) {
+        String token = "Your personal access token";
+        ApiClient apiClient = new ApiClient("apiKey", token);
+
+        CommunityAlgorithmsApi algorithmApi = apiClient.buildClient(CommunityAlgorithmsApi.class);
+
+        // Required attributes to create an algorithm
+        String name = "My Algorithm";
+        AlgorithmDto.ComputationModelEnum computationModel = AlgorithmDto.ComputationModelEnum.CLASSIC;
+
+        AlgorithmDto payload = new AlgorithmDto()
+            .name(name)
+            .computationModel(computationModel);
+        AlgorithmDto algorithm = algorithmApi.createAlgorithm(payload);
+
+        algorithm = algorithmApi.getAlgorithm(algorithm.getId());
+
+        // Retrieve a list of available application areas
+        List<TaxonomyElement> applicationAreas = algorithmApi.getApplicationAreas();
+
+        // Retrieve Engineering Science from the list
+        TaxonomyElement engineeringScience = applicationAreas.stream()
+            .filter(taxonomyElement -> "Engineering Science".equalsIgnoreCase(taxonomyElement.getLabel()))
+            .findFirst()
+            .orElseThrow();
+
+        /*
+         * Application areas have children or sub-categories e.g.
+         * Civil Engineering is a child of Engineering Science
+         * Below we show how to Civil Engineering from the list
+         */
+        TaxonomyElement civilEngineering = applicationAreas.stream()
+            .flatMap(taxonomyElement -> taxonomyElement.getChildren().stream())
+            .filter(c -> "Civil Engineering".equalsIgnoreCase(c.getLabel()))
+            .findFirst()
+            .orElseThrow();
+
+        List<String> applicationAreasUuids = new ArrayList<>();
+        applicationAreasUuids.add(engineeringScience.getUuid());
+        applicationAreasUuids.add(civilEngineering.getUuid());
+
+        /*
+         * Adds application areas to the algorithm
+         */
+
+        // Create the update request payload
+        UpdateAlgorithmRequest updateAlgorithmRequest = new UpdateAlgorithmRequest()
+            .name(name)
+            .computationModel(UpdateAlgorithmRequest.ComputationModelEnum.CLASSIC)
+            .applicationAreaUuids(applicationAreasUuids);
+        algorithm = algorithmApi.updateAlgorithm(algorithm.getId(), updateAlgorithmRequest);
+
+        //Remove all assigned application areas
+        updateAlgorithmRequest.applicationAreaUuids(new ArrayList<>());
+        algorithm = algorithmApi.updateAlgorithm(algorithm.getId(), updateAlgorithmRequest);
+
+    }
+}
---
+++ java/planqk-samples/app/src/main/java/de/stoneone/planqk/samples/AlgorithmAddAndDeleteLearningMethodsSample.java
@@ -0,0 +1,58 @@
+package de.stoneone.planqk.samples;
+
+import de.stoneone.planqk.api.CommunityAlgorithmsApi;
+import de.stoneone.planqk.api.invoker.ApiClient;
+import de.stoneone.planqk.api.model.AlgorithmDto;
+import de.stoneone.planqk.api.model.TaxonomyElement;
+import de.stoneone.planqk.api.model.UpdateAlgorithmRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgorithmAddAndDeleteLearningMethodsSample {
+
+    public static void main(String[] args) {
+        String token = "Your personal access token";
+        ApiClient apiClient = new ApiClient("apiKey", token);
+
+        CommunityAlgorithmsApi algorithmApi = apiClient.buildClient(CommunityAlgorithmsApi.class);
+
+        // Required attributes to create an algorithm
+        String name = "My Algorithm";
+        AlgorithmDto.ComputationModelEnum computationModel = AlgorithmDto.ComputationModelEnum.CLASSIC;
+
+        AlgorithmDto payload = new AlgorithmDto()
+            .name(name)
+            .computationModel(computationModel);
+        AlgorithmDto algorithm = algorithmApi.createAlgorithm(payload);
+
+        algorithm = algorithmApi.getAlgorithm(algorithm.getId());
+
+        // Retrieve a list of available learning methods
+        List<TaxonomyElement> learningMethods = algorithmApi.getLearningMethods();
+
+        // Retrieve Supervised Learning from the list
+        TaxonomyElement supervisedLearning = learningMethods.stream()
+            .filter(taxonomyElement -> "Supervised Learning".equalsIgnoreCase(taxonomyElement.getLabel()))
+            .findFirst()
+            .orElseThrow();
+
+        List<String> learningMethodUuids = new ArrayList<>();
+        learningMethodUuids.add(supervisedLearning.getUuid());
+
+        /*
+         * Adds a learning method to the algorithm
+         */
+
+        // Create the update request payload
+        UpdateAlgorithmRequest updateAlgorithmRequest = new UpdateAlgorithmRequest()
+            .name(name)
+            .computationModel(UpdateAlgorithmRequest.ComputationModelEnum.CLASSIC)
+            .learningMethodUuids(learningMethodUuids);
+        algorithm = algorithmApi.updateAlgorithm(algorithm.getId(), updateAlgorithmRequest);
+
+        //Remove all assigned learning methods
+        updateAlgorithmRequest.learningMethodUuids(new ArrayList<>());
+        algorithm = algorithmApi.updateAlgorithm(algorithm.getId(), updateAlgorithmRequest);
+
+    }
+}
